80490f361bd21e1f7a510aabc8e529b1832501e1
Swati Rani <124399483+swati910567@users.noreply.github.com>
2024-09-01 00:44:13 +0530

    Update axiosInstance.js

--- client/user-app/src/helpers/axiosInstance.js
+++ client/user-app/src/helpers/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 //import {BASE_URL} from "../constants.js"
 
-const baseURL = "https://silver-tube-backend.onrender.com";
+const baseURL = "silver-tube.vercel.app";
 export const axiosInstance = axios.create({
     baseURL,
     withCredentials: true,
