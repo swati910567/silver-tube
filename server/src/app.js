1811bce563a2769fb5c74468c86640242ce1c67a
Swati Rani <124399483+swati910567@users.noreply.github.com>
2024-09-01 00:41:30 +0530

    Update app.js

--- server/src/app.js
+++ server/src/app.js
@@ -15,8 +15,7 @@ app.use(cors({
 app.use(express.json({
     limit:"20kb"
 }))
-app.get("/",(req,res)=>{
-    res.json("Hello")})
+
 //now data from url
 app.use(express.urlencoded({extended:true,limit:"20kb"}))
 
