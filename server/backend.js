7965fe99552a9ca8f43b759434f6b87c8d5a9a9c
Swati Rani <124399483+swati910567@users.noreply.github.com>
2024-08-31 23:21:51 +0530

    Update backend.js

--- server/backend.js
+++ server/backend.js
@@ -3,20 +3,19 @@ import {app} from './src/app.js';
 import connectDB from './src/dB/index.js';
 
 dotenv.config({
-    path:'./env'
-})
+    path:'./.env'
+});
 
 
 connectDB()//async await jb implement hota h tb kuch promises bhi return krta hai 
 .then(()=>{
     app.listen((process.env.PORT||3000),()=>{
-        console.log(`Server is running at port: ${process.env.PORT}`)
+        console.log("⚙️  Server is running on Port :", process.env.PORT)
     })
 })
-.catch((error)=>{
-    console.log("Error",error)
-    throw error;
-})
+ .catch((err) => {
+    console.log("MONGODB CONNECTION FAILED!!! ", err);
+  });
 
 
 
