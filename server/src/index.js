51bc9786746390212124d1097476f7f6494a2070
Swati Rani <124399483+swati910567@users.noreply.github.com>
2024-09-01 01:06:44 +0530

    Create index.js

---
+++ server/src/index.js
@@ -0,0 +1,20 @@
+import dotenv from 'dotenv';
+import {app} from './app.js'
+import connectDB from './dB/index.js';
+
+dotenv.config({
+    path:'./env'
+})
+
+
+connectDB()//async await jb implement hota h tb kuch promises bhi return krta hai 
+.then(()=>{
+    app.listen((process.env.PORT||3000),()=>{
+        console.log(`Server is running at port: ${process.env.PORT}`)
+    })
+})
+.catch((error)=>{
+    console.log("Error",error)
+    throw error;
+})
+
